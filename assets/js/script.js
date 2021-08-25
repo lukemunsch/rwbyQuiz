61f128f1e80cbe149389763eb92e072a38cc73a7
lukemunsch <lpmunsch@gmail.com>
2021-08-25 22:54:29 +0000

    add function to hide modal on game page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
-const toggleModal = () => {
-    document.getElementsByClassName('modal').classlist.toggle('modal--hidden');
+function rulesModalUp(){
+    let showModal = document.getElementById('rules-modal');
+        showModal.removeAttribute("class", "modal--hidden");
 }
 
-document.getElementById('show-modal').addEventListener('click', toggleModal);
+function rulesModalDown(){
+    let closeModal = document.getElementById('rules-modal');
+        closeModal.setAttribute("class", "modal--hidden")   
+}
+
+document.getElementById('show-modal').addEventListener('click', toggleModalOn);
 document.getElementById('close-rules').addEventListener('click', toggleModal);
