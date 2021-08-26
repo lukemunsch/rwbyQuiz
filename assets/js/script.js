f59d88b27513b576ee749f5a7e2874649a02d2e3
lukemunsch <lpmunsch@gmail.com>
2021-08-26 21:08:25 +0000

    create more questions for quiz database

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,9 +49,212 @@ let questions = [
         answer: ["Sword-lance and shield", "Warhammer", "Twin pistols", "Rifle trident"],
         correctAnswer: 'Sword-lance and shield'
     },
+    //10 questions
+    {
+        question:"How many teammembers make up a standard team roster?",
+        answer: ["3", "4", "5", "8"],
+        correctAnswer: '4'
+    },
+    {
+        question:"Who is the headmaster of Beacon academy?",
+        answer: ["Ozpin", "Ironwood", "Taiyang", "Qrow"],
+        correctAnswer: 'Ozpin'
+    },
+    {
+        question:"What is the connection between Raven and Qrow?",
+        answer: ["Husband and Wife", "Twin Siblings", "Father and Daughter", "Mother and Son"],
+        correctAnswer: 'Twin Siblings'
+    },
+    {
+        question:"Who was next in line to be the Fall maiden?",
+        answer: ["Pyrrha", "Nora", "Yang", "Blake"],
+        correctAnswer: 'Pyrrha'
+    },
+    {
+        question:"Which Member of RWBY lived in Menagerie?",
+        answer: ["Ruby", "Weiss", "Blake", "Yang"],
+        correctAnswer: 'Blake'
+    },
+    {
+        question:"What did team RWBY receive in the post from Taiyang?",
+        answer: ["A load of washing", "A dog and his food", "Weapon repair kits", "Gushy family letters"],
+        correctAnswer: 'A dog and his food'
+    },
+    {
+        question:"Adam's surname is which member of the zodiac?",
+        answer: ["Capricorn", "Virgo", "Taurus", "Leo"],
+        correctAnswer: 'Taurus'
+    },
+    {
+        question:"Who is robbing stores for their dust supplies?",
+        answer: ["Roman", "Sun", "Cardin", "Velvet"],
+        correctAnswer: 'Roman'
+    },
+    {
+        question:"Who is the silent partner to Roman Torchwick?",
+        answer: ["Neo", "Cinder", "Emerald", "Salem"],
+        correctAnswer: 'Neo'
+    },
+    {
+        question:"Which professor has probably had too many cups of coffee?",
+        answer: ["Professor Port", "Doctor Oobleck", "Professor Goodwitch", "Professor Ozpin"],
+        correctAnswer: 'Doctor Oobleck'
+    },
+    //20 question
+
+    {
+        question:"In the Vale fighting festival, who fights against Pyrrha?",
+        answer: ["Sunn", "Emerald", "Yang", "Penny"],
+        correctAnswer: 'Penny'
+    },
+    {
+        question:"Who is the only artificial being who can generate aura?",
+        answer: ["Penny Polendina", "Nora Valyrie", "Weiss Schnee", "Cinder Fall"],
+        correctAnswer: 'Penny Polendina'
+    },
+    {
+        question:"How many gods were there when Salem los her true love?",
+        answer: ["4", "3", "2", "1"],
+        correctAnswer: '2'
+    },
+    {
+        question:"What is the main produce of Weiss' family business?",
+        answer: ["Food", "Guns", "Clothes", "Dust"],
+        correctAnswer: 'Dust'
+    },
+    {
+        question:"What is located in the vault under Haven Academy?",
+        answer: ["Staff of Creation", "Relic of Knowledge", "The Fall Maiden", "Ozpin's Favourite Books"],
+        correctAnswer: 'Relic of Knowledge'
+    },
+    {
+        question:"What is the alter ego of Maria Calavera?",
+        answer: ["Grimm Reaper", "Shorty-Pants", "Spring Maiden", "Dust Sorceress"],
+        correctAnswer: 'Grimm Reaper'
+    },
+    {
+        question:"Yang leaves to find Ruby, But who is it she is actually looking for?",
+        answer: ["Qrow", "Raven", "Robyn", "Yuma"],
+        correctAnswer: 'Raven'
+    },
+    {
+        question:"What is the team name Nora creates when rwby joins them?",
+        answer: ["JNRR", "RNJR", "NRJR", "Where's the P gone?"],
+        correctAnswer: 'JNRR'
+    },
+    {
+        question:"Where does the battle between Cinder and Pyrrha come to a dramatic end?",
+        answer: ["Amity Colusseum", "Beacon Tower", "Haven Academy", "Vale Docks"],
+        correctAnswer: 'Beacon Tower'
+    },
+    {
+        question:"What does Oscar do before Ozpin joins him?",
+        answer: ["Student", "Beggar", "Shop clerk", "Farmhand"],
+        correctAnswer: 'Farmhand'
+    },
+    //30 questions
+
     {
         question:"?",
         answer: ["", "", "", ""],
         correctAnswer: ''
     },
+    {
+        question:"?",
+        answer: ["", "", "", ""],
+        correctAnswer: ''
+    },
+    {
+        question:"?",
+        answer: ["", "", "", ""],
+        correctAnswer: ''
+    },
+    {
+        question:"?",
+        answer: ["", "", "", ""],
+        correctAnswer: ''
+    },
+    {
+        question:"?",
+        answer: ["", "", "", ""],
+        correctAnswer: ''
+    },
+    {
+        question:"?",
+        answer: ["", "", "", ""],
+        correctAnswer: ''
+    },
+    {
+        question:"?",
+        answer: ["", "", "", ""],
+        correctAnswer: ''
+    },
+    {
+        question:"?",
+        answer: ["", "", "", ""],
+        correctAnswer: ''
+    },
+    {
+        question:"?",
+        answer: ["", "", "", ""],
+        correctAnswer: ''
+    },
+    {
+        question:"?",
+        answer: ["", "", "", ""],
+        correctAnswer: ''
+    },
+    // 40 questions
+
+    {
+        question:"?",
+        answer: ["", "", "", ""],
+        correctAnswer: ''
+    },
+    {
+        question:"?",
+        answer: ["", "", "", ""],
+        correctAnswer: ''
+    },
+    {
+        question:"?",
+        answer: ["", "", "", ""],
+        correctAnswer: ''
+    },
+    {
+        question:"?",
+        answer: ["", "", "", ""],
+        correctAnswer: ''
+    },
+    {
+        question:"?",
+        answer: ["", "", "", ""],
+        correctAnswer: ''
+    },
+    {
+        question:"?",
+        answer: ["", "", "", ""],
+        correctAnswer: ''
+    },
+    {
+        question:"?",
+        answer: ["", "", "", ""],
+        correctAnswer: ''
+    },
+    {
+        question:"?",
+        answer: ["", "", "", ""],
+        correctAnswer: ''
+    },
+    {
+        question:"?",
+        answer: ["", "", "", ""],
+        correctAnswer: ''
+    },
+    {
+        question:"?",
+        answer: ["", "", "", ""],
+        correctAnswer: ''
+    },
+    //50 questions
 ]
