07e3bcf2ea8639cec1d34c7adf9abc020be2a3fa
lukemunsch <lpmunsch@gmail.com>
2021-08-26 20:30:50 +0000

    add questions to javascript file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,57 @@
-function modalChange(){
-    let modDown = document.getElementById('rules-modal');
-    if(this.getAttribute("class" === "modal--hidden")){
-        modDown.removeAttribute("class", "modal--hidden");
-    } else {
-        modDown.setAttribute("class", "modal--hidden");
-    }
-}
+let questions = [
+    {
+        question: "What body part did Yang lose in her first meeting with Adam?",
+        answer: ["Right arm", "Left leg", "Her hair", "Wait... WHAT!!!"],
+        correctAnswer: 'Right arm'
+    },
+    {
+        question: "What is the nickname given to Jaune Arc in the first episode?",
+        answer: ["Slugger", "Ladies-Man", "Vomit-Boy", "Useless-stick"],
+        correctAnswer: 'Vomit-boy'
+    },
+    {
+        question: "What happened to the moon?",
+        answer: ["Always been like that", "Gods destroyed it", "Grimm Attack", "Asteroid Impact"],
+        correctAnswer: 'Gods destroyed it'
+    },
+    {
+        question:"Which character is not based on a Fairy Tale?",
+        answer: ["Ren", "Ruby", "Hazel", "Weis"],
+        correctAnswer: 'Ren'
+    },
+    {
+        question:"Which character does not use a scythe weapon?",
+        answer: ["Ruby Rose", "Qrow Branwen", "Maria Calavera", "Glynda Goodwitch"],
+        correctAnswer: 'Glynda Goodwitch'
+    },
+    {
+        question:"Which locker belongs to Jaune?",
+        answer: ["616", "636", "239", "345"],
+        correctAnswer: '636'
+    },
+    {
+        question:"Where was Sunn raised?",
+        answer: ["Mistral", "Vacuo", "Mantle", "Vale"],
+        correctAnswer: 'Vacuo'
+    },
+    {
+        question:"Who is the mother of Ruby?",
+        answer: ["Summer", "Willow", "Kali", "rAVEN"],
+        correctAnswer: 'Summer'
+    },
+    {
+        question:"Who is the odd One out?",
+        answer: ["ozma", "Oscar", "Ozpin", "Oobleck"],
+        correctAnswer: 'Oobleck'
+    },
+    {
+        question:"Pyrrha Nikos' weapon is what?",
+        answer: ["Sword-lance and shield", "Warhammer", "Twin pistols", "Rifle trident"],
+        correctAnswer: 'Sword-lance and shield'
+    },
+    {
+        question:"?",
+        answer: ["", "", "", ""],
+        correctAnswer: ''
+    },
+]
